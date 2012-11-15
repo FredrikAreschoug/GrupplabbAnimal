b2a9432a4ae0e39f19a5f08264fc68549410a145
Christopher Lindblom <cmc.lindblom@gmail.com>
2012-11-15 14:32:29 +0100

    Lagt till sound, eat och bunnyJump metoder i Bunny

--- GrupplabbAnimal/src/animal/Bunny.java
+++ GrupplabbAnimal/src/animal/Bunny.java
@@ -2,4 +2,24 @@ package animal;
 
 public class Bunny {
 
+	private int bunnyEnergy;
+	
+	public Bunny() {
+		bunnyEnergy=10;
+	}
+	
+	public String sound() {
+		bunnyEnergy--;
+		return "Pip, pip, gnag!";
+	}
+	
+	public int eat() {
+		bunnyEnergy += 5;
+		return bunnyEnergy;
+	}
+	
+	public void bunnyJump() {
+		System.out.println("Hopp! Tjooo!");
+		bunnyEnergy -= 2;
+	}
 }
