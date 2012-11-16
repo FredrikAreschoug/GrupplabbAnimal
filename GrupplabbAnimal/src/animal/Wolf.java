eddb2efdc66a51f7e2e8339b0bc38c1845f61f6e
Ivan <ivan.a.hook@gmail.com>
2012-11-16 11:26:09 +0100

    lite aska bara

--- GrupplabbAnimal/src/animal/Wolf.java
+++ GrupplabbAnimal/src/animal/Wolf.java
@@ -9,7 +9,7 @@ public class Wolf extends Stats{
 		energy = 10;
 		attackPower = 10;
 		speed = 10;
-		System.out.println("I like bunnies...yum yum");
+//		System.out.println("I like bunnies...yum yum");
 	}
 	public String sound(){
 		String fx = "AAAoooooou";
